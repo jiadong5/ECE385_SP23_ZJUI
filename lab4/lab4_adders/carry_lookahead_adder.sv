157d0850c91ac7dc9268cab40193278d2edfc9b0
Autentico203 <hanggang.20@intl.zju.edu.cn>
2023-03-08 11:55:15 +0800

    add carry_lookahead_adder 4 bits

--- lab4/lab4_adders/carry_lookahead_adder.sv
+++ lab4/lab4_adders/carry_lookahead_adder.sv
@@ -6,10 +6,38 @@ module carry_lookahead_adder
     output  logic           CO
 );
 
-    /* TODO
-     *
-     * Insert code here to implement a CLA adder.
-     * Your code should be completly combinational (don't use always_ff or always_latch).
-     * Feel free to create sub-modules or other files. */
      
+endmodule
+
+module carry_lookahead_adder4
+(
+    input logic[3:0] A,
+    input logic[3:0] B,
+    input logic Cin,
+    output logic[3:0] Sum,
+    output logic CO,
+    output logic PG,
+    output logic GG
+);
+
+    logic[3:0] C; // Carry
+    logic[3:0] P;
+    logic[3:0] G;
+
+    always_comb 
+    begin
+        P = A ^ B;
+        G = A & B;
+        C[0] = Cin;
+        C[1] = Cin & P[0] | G[0];
+        C[2] = Cin & P[0] & P[1] | G[0] & P[1] | G[1];
+        C[3] = Cin & P[0] & P[1] & P[2] | G[0] & P[1] & P[2] | G[1] & P[2] | G[2];
+
+        Sum = A ^ B ^ C;
+        CO = Cin & P[0] & P[1] & P[2] & P[3] | G[0] & P[1] & P[2] & P[3] | G[1] & P[2] & P[3] | G[2] & P[3] | G[3];
+        PG = P[0] & P[1] & P[2] & P[3];
+        GG = G[3] | G[2] & P[3] | G[1] & P[3] & P[2] | G[0] & P[3] & P[2] & P[1];
+
+    end
+
 endmodule
