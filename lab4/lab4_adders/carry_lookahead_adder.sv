02251889fc42473a50dee697fa89e8024566f674
Autentico203 <hanggang.20@intl.zju.edu.cn>
2023-03-08 22:37:52 +0800

    finish carry lookahead adder

--- lab4/lab4_adders/carry_lookahead_adder.sv
+++ lab4/lab4_adders/carry_lookahead_adder.sv
@@ -1,3 +1,5 @@
+
+
 module carry_lookahead_adder
 (
     input   logic[15:0]     A,
@@ -15,13 +17,13 @@ module carry_lookahead_adder
     logic[4:1] unused_C;
 
     // Set C4[3:0] for 4bit CLA adders, output CO.
-    carry_lookahead_unit16(.PG(PG), .GG(GG), .Cin(1'b0), .CO(CO), .C4(C4));
+    carry_lookahead_unit16 CLU(.PG(PG), .GG(GG), .Cin(1'b0), .CO(CO), .C4(C4));
 
     // Get the value of PG,GG
     carry_lookahead_adder4 CLA0(.A(A[3:0]), .B(B[3:0]), .Cin(1'b0), .Sum(Sum[3:0]), .CO(unused_C[1]), .PG(PG[0]), .GG(GG[0]) );
-    carry_lookahead_adder4 CLA1(.A(A[7:4]), .B(B[7:4]), .Cin(C4[1]), .Sum(Sum[7:4]), .CO(unused_C[2]), .PG(PG[1]), .GG(GG[1]) );
-    carry_lookahead_adder4 CLA2(.A(A[11:8]), .B(B[11:8]), .Cin(C4[2]), .Sum(Sum[11:8]), .CO(unused_C[3]), PG(PG[2]), .GG(GG[2]) );
-    carry_lookahead_adder4 CLA3(.A(A[15:12]), .B(B[15:12]), .Cin(C4[3]), .Sum(Sum[15:12]), .CO(unused_C[4]), PG(PG[3]), .PG(PG[3]));
+    carry_lookahead_adder4 CLA1(.A(A[7:4]), .B(B[7:4]), .Cin(C4[0]), .Sum(Sum[7:4]), .CO(unused_C[2]), .PG(PG[1]), .GG(GG[1]) );
+    carry_lookahead_adder4 CLA2(.A(A[11:8]), .B(B[11:8]), .Cin(C4[1]), .Sum(Sum[11:8]), .CO(unused_C[3]), .PG(PG[2]), .GG(GG[2]) );
+    carry_lookahead_adder4 CLA3(.A(A[15:12]), .B(B[15:12]), .Cin(C4[2]), .Sum(Sum[15:12]), .CO(unused_C[4]), .PG(PG[3]), .GG(GG[3]));
      
 endmodule
 
@@ -33,7 +35,7 @@ module carry_lookahead_adder4
     output logic[3:0] Sum,
     output logic CO,
     output logic PG,
-    output logc GG
+    output logic GG
 );
 
     // Input to lookahead unit
@@ -69,6 +71,8 @@ module carry_lookahead_unit4
     output logic GG
 );
 
+    always_comb
+    begin
     C[0] = Cin;
     C[1] = Cin & P[0] | G[0];
     C[2] = Cin & P[0] & P[1] | G[0] & P[1] | G[1];
@@ -78,6 +82,7 @@ module carry_lookahead_unit4
 
     PG = P[0] & P[1] & P[2] & P[3];
     GG = G[3] | G[2] & P[3] | G[1] & P[3] & P[2] | G[0] & P[3] & P[2] & P[1];
+    end
 
 endmodule
 
@@ -88,13 +93,16 @@ module carry_lookahead_unit16
     input logic[3:0] GG,
     input logic Cin,
 
-    output logic CO;
-    output logic[3:0] C4,
-)
+    output logic CO,
+    output logic[3:0] C4 // C4,C8,C12
+);
+    always_comb
+    begin
 
     C4[0] = GG[0] | Cin & PG[0];
     C4[1] = GG[1] | GG[0] & PG[1] | Cin & PG[0] & PG[1];
     C4[2] = GG[2] | GG[1] & PG[2] | GG[0] & PG[2] & PG[1] |  Cin & PG[2] & PG[1] & PG[0];
-    C4[3] = GG[3] | GG[2] & PG[3] | GG[1] & PG[3] & PG[2] | GG[0] & PG[3] & PG[2] & PG[1] | Cin & PG[3] & PG[2] & PG[1] & PG[0];
+    CO = GG[3] | GG[2] & PG[3] | GG[1] & PG[3] & PG[2] | GG[0] & PG[3] & PG[2] & PG[1] | Cin & PG[3] & PG[2] & PG[1] & PG[0];
+    end
 
 endmodule
