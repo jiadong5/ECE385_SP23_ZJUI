a2a72259c868109f866e16df918b8abc674e893e
Autentico203 <hanggang.20@intl.zju.edu.cn>
2023-03-14 22:05:28 +0800

    Add: register unit

--- lab5/src/register_unit.sv
+++ lab5/src/register_unit.sv
@@ -38,9 +38,23 @@ module dreg (input Clk, Load, Reset, D,
             else
                 Q <= Q;
     end
-    
-
 
 endmodule
 
 
+module register_unit
+(
+    input logic Clk, Reset, Ashift_In, Bshift_In, Ld_A, Ld_B, Shift_En,
+    input logic[7:0] D,
+    output logic B_out,  // B_out aka M
+    output logic[7:0] AData_out, BData_out
+);
+
+    logic shift_bit;
+    reg_8 regA(.*, .Shift_In(Ashift_In), .Load(Ld_A),
+                .Shift_Out(shift_bit), .Data_Out(AData_out));
+    reg_8 regB(.*, .Shift_In(shift_bit), .Load(Ld_B),
+                .Shift_Out(B_Out), .Data_Out(BData_out));
+
+
+endmodule
