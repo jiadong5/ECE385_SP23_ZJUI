74fe1f63c47b0b023c52bbeb84768bab34040683
Ayanami Chicken <jiadong.20@intl.zju.edu.cn>
2023-03-22 22:53:21 +0800

    Complete Undebugged Week1 assignment
    Update MUX and Datapath,
    Mainly completed MEM2IO and PC updating registers and MUXes.

--- lab6/src/MUX.sv
+++ lab6/src/MUX.sv
@@ -2,12 +2,35 @@ module MUX_IO (
     input logic[15:0] Data_to_CPU,
     input logic[15:0] Data_Bus,
     input logic MIO_EN,
-    output logic[15:0] MDR_In,
+    output logic[15:0] MDR_Input
+);
+//
+
+always_comb
+begin
+    if(~MIO_EN)
+        MDR_Input = Data_Bus;
+    else    
+        MDR_Input = Data_to_CPU;
+end
+endmodule
+
+
+module MUX_PC (
+    input logic[15:0] Data_Bus,
+    input logic[15:0] Data_Calc,
+    input logic[15:0] PCPP,
+    input logic[1:0] PCMUX,
+    output logic[15:0] PC_Input
 );
 
 always_comb
 begin
-    case(MIO_EN)
-    1'b0: out = 
-
+    case (PCMUX)
+        default: PC_Input = PCPP;
+        2'b10: PC_Input = Data_Calc;
+        2'b11: PC_Input = Data_Bus;
+    endcase
+end
 endmodule
+
--- lab6/src/datapath.sv
+++ lab6/src/datapath.sv
@@ -2,7 +2,7 @@ module datapath(
     input logic Clk, 
     input logic Reset,
     input logic LD_MAR, LD_MDR, LD_IR, LD_BEN, LD_CC, LD_REG, LD_PC, LD_LED,
-    input logic GatePC, GateMDR, GateALU,GateMARMUX,
+    input logic GatePC, GateMDR, GateALU, GateMARMUX,
 	input logic DRMUX, SR1MUX, SR2MUX, ADDR1MUX, MIO_EN,
     input logic [15:0] MDR_In,
 	input logic [1:0]  ADDR2MUX, ALUK, PCMUX,
@@ -14,16 +14,42 @@ module datapath(
     output logic [15:0] PC
 );
 
-// Circuit flow Assignments
-// TODO: To implement LC-3 Circuit Diagram.
+/*--Define local variables--*/
 
-// Define Registers and MUXes.
+// used in MEM2IO
+logic[15:0] Data_Bus,
+            MDR_Input,
+            MAR_Input,
+            Data_from_CPU, 
+            Data_to_CPU;
 
+//  Used in PC
+logic[15:0] Data_Calc;
+logic[15:0] PCPP;
+logic[15:0] PC_Input;
 
+/*--Ckt assignments--*/
 
-// Week 1 part of the circuit.
-// I/O from SRAM part.
-Mem2IO      MENM2IO;
-tristate    TRISTATE;
+// assign circuits in MEM2IO
+assign Data_to_CPU = MDR_In
+assign Data_Bus = MDR;
+assign MAR_Input = Data_Bus;
+assign Data_from_CPU = MDR;
+
+// assign circuits in PC
+assign Data_Bus = PC;
+assign PCPP = PC + 1;
+assign Data_Bus = Data_Calc;
+
+/*--Define Registers and MUXes.--*/
+
+// Registers
+register #(.N(16)) MDR_reg(.*, .load(LD_MDR), .Data_In(MDR_Input), .Data_Out(MDR));
+register #(.N(16)) MAR_reg(.*, .load(LD_MAR), .Data_In(MAR_Input), .Data_Out(MAR));
+register #(.N(16)) PC_reg(.*, .load(LD_PC), .Data_In(PC_Input), .Data_Out(PC));
+
+// MUXes
+MUX_IO MUX_to_MDR(.*);
+MUX_PC MUX_to_PCR(.*);
 
 endmodule
