66be126eb1af1c0115f9bb23763850cde650fcce
hthuz <hanggang.20@intl.zju.edu.cn>
2023-05-10 23:04:09 +0800

    Add: player move front walk steps. Walk too fast and need to be slow down

--- boxhead/sv/playerROM.sv
+++ boxhead/sv/playerROM.sv
@@ -11,16 +11,19 @@ module playerROM
 		output logic [4:0] data_Out
 );
 
-// For directions, each direction 40 * 64
-// mem has width of 3 bits and a total of 40 * 64 * 4
-logic [4:0] mem [0:10239];
+// Four directions, each direction 40 * 64 and 3 walk steps
+// mem has width of 3 bits and a total of 40 * 64 * 12
+logic [4:0] mem [0:30719];
 
 initial
 begin
-    $readmemh("sprite/player_front.txt", mem, 0);
-    $readmemh("sprite/player_left.txt", mem, 1 * 2560);
-    $readmemh("sprite/player_back.txt", mem, 2 * 2560);
-    $readmemh("sprite/player_right.txt", mem, 3 * 2560);
+    $readmemh("sprite/player_front0.txt", mem, 0);
+    $readmemh("sprite/player_front1.txt",mem, 1 * 2560);
+    $readmemh("sprite/player_front2.txt",mem, 2 * 2560);
+
+    $readmemh("sprite/player_left.txt", mem, 3 * 2560);
+    $readmemh("sprite/player_back.txt", mem, 4 * 2560);
+    $readmemh("sprite/player_right.txt", mem, 5 * 2560);
 end
 
 
