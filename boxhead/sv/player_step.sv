50dbe403a50b824e5371b65c5c4558f13dd4a195
hthuz <hanggang.20@intl.zju.edu.cn>
2023-05-12 21:28:03 +0800

    Add: player walk FSM

---
+++ boxhead/sv/player_step.sv
@@ -0,0 +1,49 @@
+
+
+module player_step ( input logic Clk,
+    Reset,
+    output logic [1:0] Obj_Step_Count
+);
+
+    enum logic [1:0] { S_0,
+    S_1,
+    S_2,
+        S_3} State, Next_state;
+
+    always_ff @ (posedge Clk)
+    begin
+        if (Reset)
+            State <= S_0;
+        else 
+            State <= Next_state;
+    end
+
+
+    always_comb
+    begin
+        // assign next state
+        unique case (State)
+            S_0 :
+                Next_state = S_1;
+            S_1 :
+                Next_state = S_2;
+            S_2 : 
+                Next_state = S_3;
+            S_3 :
+                Next_state = S_0;
+
+        // assign output
+            case (State)
+                S_0 :
+                    Obj_Step_Count = 2'd0;
+                S_1 :
+                    Obj_Step_Count = 2'd1;
+                S_2 :
+                    Obj_Step_Count = 2'd2;
+                S_3 :
+                    Obj_Step_Count = 2'd3;
+                default : ;
+            endcase
+    end
+
+endmodule
